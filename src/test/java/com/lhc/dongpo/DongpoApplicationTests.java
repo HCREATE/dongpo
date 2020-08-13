5c657568ee20dc426a7328e3bcd200ccbee58a8e
haojt <haojiangtao1995@126.com>
2020-08-13 10:40:11 +0800

    init

---
+++ src/test/java/com/lhc/dongpo/DongpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhc.dongpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongpoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
