cebf640225e882582dfd268ab0cd4fabaa5167f2
haojt <haojiangtao1995@126.com>
2021-01-27 10:17:29 +0800

    concurrent

--- src/main/java/com/lhc/dongpo/concurrent/Print.java
+++ src/main/java/com/lhc/dongpo/concurrent/Print.java
@@ -33,7 +33,7 @@ public class Print {
     */
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Object o = new Object();
 
         new Thread(new PrintRunnable(o, 1)).start();
@@ -43,21 +43,21 @@ public class Print {
 
 }
 
-class PrintRunnable implements Runnable{
+class PrintRunnable implements Runnable {
     private final Object o;
     private int threadId;
     private static volatile int num = 1;
 
-    PrintRunnable(Object o, int threadId){
+    PrintRunnable(Object o, int threadId) {
         this.o = o;
         this.threadId = threadId;
     }
 
     @Override
     public void run() {
-        while(num < 65){
-            synchronized (o){
-                while(num/5%3 + 1 != threadId){
+        while (num < 65) {
+            synchronized (o) {
+                while (num / 5 % 3 + 1 != threadId) {
                     try {
                         o.wait();
                     } catch (InterruptedException e) {
@@ -65,10 +65,10 @@ class PrintRunnable implements Runnable{
                     }
                 }
 
-                System.out.print("线程"+threadId+": ");
-                for(int i=0; i<5; i++) {
+                System.out.print("线程" + threadId + ": ");
+                for (int i = 0; i < 5; i++) {
                     System.out.print(num + ",");
-                    num ++;
+                    num++;
                 }
                 System.out.println();
 
--- src/main/java/com/lhc/dongpo/concurrent/ShareDataV1.java
+++ src/main/java/com/lhc/dongpo/concurrent/ShareDataV1.java
@@ -11,44 +11,43 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Date: 2021-01-11
  * Time: 16:03
  */
+
 /**
- *  问题：生产者消费者模型，是互联网应用模型中比较经典的模型。
- *  要求实现这一模型，并实例化3个生产者和3个消费者，保证在不主动停止程序的前提下，生产和消费能够一直进行
+ * 问题：生产者消费者模型，是互联网应用模型中比较经典的模型。
+ * 要求实现这一模型，并实例化3个生产者和3个消费者，保证在不主动停止程序的前提下，生产和消费能够一直进行
  * （即当消费者无商品可消费的时候，等待生产者生产；生产者到达生产上限时候，停止生产），注意考虑线程安全。编程语言不限。
-*/
+ */
 public class ShareDataV1 {
     private static final int MAX_CAPACITY = 10; //阻塞队列容量
-    private static BlockingQueue<Integer> blockingQueue= new ArrayBlockingQueue<>(MAX_CAPACITY); //阻塞队列
-    private  volatile boolean FLAG = true;
+    private static BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<>(MAX_CAPACITY); //阻塞队列
+    private volatile boolean FLAG = true;
     private AtomicInteger atomicInteger = new AtomicInteger();
 
     public void produce() throws InterruptedException {
-        while (FLAG){
+        while (true) {
             boolean retvalue = blockingQueue.offer(atomicInteger.incrementAndGet(), 2, TimeUnit.SECONDS);
-            if (retvalue==true){
-                System.out.println(Thread.currentThread().getName()+"\t 插入队列"+ atomicInteger.get()+"成功"+"资源队列大小= " + blockingQueue.size());
-            }else {
-                System.out.println(Thread.currentThread().getName()+"\t 插入队列"+ atomicInteger.get()+"失败"+"资源队列大小= " + blockingQueue.size());
+            if (retvalue == true) {
+                System.out.println(Thread.currentThread().getName() + "\t 插入队列" + atomicInteger.get() + "成功" + "资源队列大小= " + blockingQueue.size());
+            } else {
+                System.out.println(Thread.currentThread().getName() + "\t 插入队列" + atomicInteger.get() + "失败" + "资源队列大小= " + blockingQueue.size());
 
             }
             TimeUnit.SECONDS.sleep(1);
         }
-        System.out.println(Thread.currentThread().getName()+"FLAG变为flase，生产停止");
     }
 
     public void consume() throws InterruptedException {
         Integer result = null;
-        while (true){
+        while (true) {
             result = blockingQueue.poll(2, TimeUnit.SECONDS);
-            if (null==result){
+            if (null == result) {
                 Thread.sleep(5000);
             }
-            System.out.println(Thread.currentThread().getName()+"\t 消费"+ result+"成功"+"\t\t"+"资源队列大小= " + blockingQueue.size());
+            System.out.println(Thread.currentThread().getName() + "\t 消费" + result + "成功" + "\t\t" + "资源队列大小= " + blockingQueue.size());
             Thread.sleep(1500);
         }
 
     }
-
 
 
     public static void main(String[] args) {
